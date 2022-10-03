5418000f108497051983dfc2e4e938bedbbdda70
virrenpatel <virenpatel24611@gmail.com>
2022-10-03 19:21:06 +0100

    My homework week 5

---
+++ src/Homework_Week5_Viren/Class10.java
@@ -0,0 +1,15 @@
+package Homework_Week5_Viren;
+
+public class Class10 {
+    public static void main(String [] args) {
+        int a = 25;
+        int b = 5;
+        String name = "input First number  ";
+        System.out.print(name);
+        System.out.println( a);
+        String name1 = "input Second number  ";
+        System.out.print(name1);
+        System.out.println( b);
+        System.out.println("25*5 = 125");
+}
+}
